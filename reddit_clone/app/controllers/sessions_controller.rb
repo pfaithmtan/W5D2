8de6e8380d19bf6624997e2790ac47d49e5bd215
Faith <pfaithmtan@gmail.com>
2018-09-18 11:48:37 -0700

    Create SessionsController

---
+++ reddit_clone/app/controllers/sessions_controller.rb
@@ -0,0 +1,27 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    @user = User.find_by_credentials(
+      params[:user][:username],
+      params[:user][:password]
+    )
+
+    if @user
+      login(@user)
+      # redirect_to link_url
+    else
+      flash.[:errors] = ["Invalid Username and Password!"]
+      redirect_to new_session_url
+    end
+  end
+
+  def destroy
+    logout
+    redirect_to new_session_url
+  end
+
+end
